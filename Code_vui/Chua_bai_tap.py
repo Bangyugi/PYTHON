d0f4b9b53393597a45e56071500f5744e8681c04
Bang Van <bangtranvan08@gmail.com>
2024-01-28 22:26:21 +0700

    update

---
+++ Code_vui/Chua_bai_tap.py
@@ -0,0 +1,16 @@
+
+
+character_m  = 'm'
+print (character_m, type(character_m))
+# Chuyển ký tự sang số
+interger_m = ord(character_m)
+print(interger_m, type(interger_m))
+
+interger_n = interger_m +1
+print(interger_n, type(interger_n))
+# Chuyển số sang ký tự
+character_n = chr(interger_n)
+print (character_n, type(character_n))
+
+
+
